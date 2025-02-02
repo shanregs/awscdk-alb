5f50e3c4f9189d4de2475c43cf9f6cbbee109623
shanregs <shanregs@gmail.com>
2025-02-02 18:34:09 +0530

    Initial commit

---
+++ test/albstackproject.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Albstackproject from '../lib/albstackproject-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/albstackproject-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Albstackproject.AlbstackprojectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
