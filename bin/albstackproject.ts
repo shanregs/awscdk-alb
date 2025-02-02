cd7a387f9a0d805dbfd8fd0c9f6556d2e8c1f38c
shanregs <shanregs@gmail.com>
2025-02-02 21:18:19 +0530

    first commit

--- bin/albstackproject.ts
+++ bin/albstackproject.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { AlbstackprojectStack } from '../lib/albstackproject-stack';
+import { AlbStackProjectStack } from '../lib/albstackproject-stack';
 
 const app = new cdk.App();
-new AlbstackprojectStack(app, 'AlbstackprojectStack', {
+new AlbStackProjectStack(app, 'AlbstackprojectStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
